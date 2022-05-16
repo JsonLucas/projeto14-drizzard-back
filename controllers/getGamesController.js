5b21e22e0709a5a7e533c443303d6eb68b30b9bd
Jason <jasonlucas443@gmail.com>
2022-05-15 23:53:52 -0300

    feat: route for getting a single game information implemented

--- controllers/getGamesController.js
+++ controllers/getGamesController.js
@@ -1,6 +1,6 @@
-import { getGames } from "../database/actions.js";
+import { getGames, getSingleGame } from "../database/actions.js";
 
-const getGamesController = async (req, res) => {
+export const getGamesController = async (req, res) => {
     try{
         const response = await getGames();
         res.status(200).send(response);
@@ -10,4 +10,18 @@ const getGamesController = async (req, res) => {
     }
 }
 
-export default getGamesController;
+export const getSingleGameController = async (req, res) => {
+    try{
+        const { _id } = req.headers;
+        const response = await getSingleGame({_id});
+        if(response){
+            res.status(200).send(response);
+        }else{
+            res.status(404).send('jogo não encontrado.');
+        }
+        return;
+    }catch(e){
+        console.log(e.message);
+        res.status(500).send('internal error.')
+    }
+}
