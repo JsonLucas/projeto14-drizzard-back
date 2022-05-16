ccafb91db25582fc88ed7803731c9d70d8c50af7
Jason <jasonlucas443@gmail.com>
2022-05-16 10:07:44 -0300

    feat: logout feature implemented

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import gamesRoute from "./gamesRoute/gamesRoute.js";
 import signInRouter from "./loginRoutes/signInRoute.js";
+import logoutRoute from "./logoutRoute/logoutRoute.js";
 import signUpRouter from "./signUpRoute/signUpRoute.js";
 
 const router = Router();
 router.use(signInRouter);
 router.use(signUpRouter);
 router.use(gamesRoute);
+router.use(logoutRoute);
 
 export default router;
